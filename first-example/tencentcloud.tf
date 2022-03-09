1d5b188327290c07abd8787c9d79807cdb2270c6
KaliArch <18329903316@163.com>
2022-03-09 19:00:12 +0800

    Update tencentcloud.tf

--- first-example/tencentcloud.tf
+++ first-example/tencentcloud.tf
@@ -4,3 +4,13 @@ provider "tencentcloud" {
   region     = ""
 }
 
+
+terraform {
+  required_providers {
+    tencentcloud = {
+      source  = "registry.terraform.io/tencentcloudstack/tencentcloud"
+
+    }
+  }
+
+}
